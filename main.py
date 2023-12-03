ceee094b2fb7426baf7f1ea558010028a7b6ce57
Franck NOUTSA FOTSO <fotsofranck237@gmail.com>
2023-12-03 19:08:18 +0100

    Added PizzaPersonnalisee SUBCLASS + new functionalities

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from pizza import Pizza
+from pizza import *
 
 # veggie ;
 #    By default -> False
 #    True -> La Pizza est végétarienne
 
-pizzas = (Pizza("Cannibale", 12.6, ("viande hachée", "poulet roti", "merguez", "mozzarella")),
-          Pizza("Savoyarde", 10.2, ("mozzarella", "lardons fumés", "pomme de terre", "reblochon")),
+pizzas = [Pizza("Cannibale", 12.6, ("viande hachée", "poulet roti", "merguez")),
+          Pizza("Savoyarde", 10.2, ("mozzarella", "lardons fumés", "pomme de terre", "reblochon", "origan")),
           Pizza("Végétarienne", 7.8, ("champignons", "tomates", "oignons", "poivrons"), True),
           Pizza("Hot & Spicy", 11.3, ("piment jalapenos", "merguez", "tomates", "duo de poivrons")),
-          Pizza("4 Fromages", 8.5, ("brie", "emmental", "compté", "parmessan"), True))
+          Pizza("4 Fromages", 8.5, ("brie", "emmental", "compté", "parmessan"), True),
+          PizzaPersonnalisee()]
 
 for pizza in pizzas:
     pizza.afficher()
 
-# ------ FILTRES ------
+# ------ FILTRES ------ #
 """
 for pizza in pizzas:
     if pizza.prix < 10:  # Condition pour n'afficher que les Pizzas à moins de 10€
@@ -30,4 +31,14 @@ for pizza in pizzas:
 for pizza in pizzas:
     if pizza.veggie:  # Condition pour n'afficher que les Pizzas végétarienne # Ajoute le if not pour afficher les non veggie
         pizza.afficher() 
+"""
+
+# ------ TRI PIZZA ------ #
+"""
+def tri(e):
+    return e.nom  # L'element e dans ma liste de pizzas c'est bien une Pizza - Ici on tri le nom avec ".nom"
+    # return e.prix  # Ici on tri en fonction du prix
+    # return len(e.ingredients)  # Triage par nombre d'ingredients
+
+pizzas.sort(key=tri)
 """
--- pizza.py
+++ pizza.py
@@ -14,3 +14,24 @@ class Pizza:
         print(", ".join(self.ingredients))
 
         print()
+
+
+class PizzaPersonnalisee(Pizza):
+    PRIX_DE_BASE = 7
+    PRIX_PAR_INGREDIENT = 1.2
+
+    def __init__(self, prix=PRIX_DE_BASE):
+        super().__init__("Personnalisée", prix, [])
+        self.demander_ingredients_utilisateur()
+        self.calculer_le_prix()
+
+    def demander_ingredients_utilisateur(self):
+        while True:
+            ingredient = input("Ajoutez un ingrédient (ou ENTER pour terminer) : ")
+            if ingredient == "":
+                return
+            self.ingredients.append(ingredient)
+            print(f"Vous avez {len(self.ingredients)} ingrédient(s) : {', '.join(self.ingredients)}")
+
+    def calculer_le_prix(self):
+        self.prix = self.PRIX_DE_BASE + (self.PRIX_PAR_INGREDIENT*len(self.ingredients))
