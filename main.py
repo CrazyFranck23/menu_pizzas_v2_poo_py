65b8a17485f085532245e873a0e4a414c68c54df
Franck NOUTSA FOTSO <fotsofranck237@gmail.com>
2023-12-03 17:12:46 +0100

    Added ingredients variable in class Pizza, added a list of pizza and filled it

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from pizza import Pizza
 
-pizza1 = Pizza("Cannibale", 12.6)
-pizza1.afficher()
+pizzas = (Pizza("Cannibale", 12.6, ("viande hachée", "poulet roti", "merguez", "mozzarella")),
+          Pizza("Savoyarde", 10.2, ("mozzarella", "lardons fumés", "pomme de terre", "reblochon")),
+          Pizza("Hot & Spicy", 11.3, ("piment jalapenos", "merguez", "tomates", "duo de poivrons")),
+          Pizza("4 Fromages", 8.5, ("brie", "emmental", "compté", "parmessan")),
+          Pizza("Végétarienne", 8.5, ("champignons", "tomates", "oignons", "poivrons")))
+
+for pizza in pizzas:
+    pizza.afficher()
+
--- pizza.py
+++ pizza.py
@@ -1,8 +1,12 @@
 
 class Pizza:
-    def __init__(self, nom, prix):
+    def __init__(self, nom, prix, ingredients):
         self.nom = nom
         self.prix = prix
+        self.ingredients = ingredients
 
     def afficher(self):
         print(f"PIZZA {self.nom} : {self.prix}€")
+        print(", ".join(self.ingredients))
+        print()
+
